d678a62906302d8c1971af559187357e09d8bdeb
SebastianJL <johannes.lade@yahoo.com>
2020-08-02 00:39:16 +0200

    Improve AddStudyPlanForm.
    - Wrap Form in SingleChildScrollView to avoid pixel overflow on keyboard.
    - Wrap elements with ListTile for better spacing.
    - Replace InputDatePickerFormField with DateTimeField.

--- lib/src/forms.dart
+++ lib/src/forms.dart
@@ -1,10 +1,11 @@
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:study_planer/src/blocs/study_plan_cubit.dart';
 import 'package:study_planer/src/models.dart';
 
 class AddStudyPlanForm extends StatefulWidget {
-
   @override
   AddStudyPlanFormState createState() {
     return AddStudyPlanFormState();
@@ -18,7 +19,11 @@ class AddStudyPlanFormState extends State<AddStudyPlanForm> {
   // Note: This is a `GlobalKey<FormState>`,
   // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
-  StudyPlan _newStudyPlan = StudyPlan.empty();
+  final StudyPlan _newStudyPlan = StudyPlan.empty();
+  final _format = DateFormat("yyyy-MM-dd");
+  final _initialDate = DateTime.now();
+  final _firstDate = DateTime(2000);
+  final _lastDate = DateTime.now().add(Duration(days: 2 * 365));
   StudyPlanCubit cubit;
 
   @override
@@ -26,40 +31,56 @@ class AddStudyPlanFormState extends State<AddStudyPlanForm> {
     cubit = BlocProvider.of<StudyPlanCubit>(context);
     return Form(
       key: _formKey,
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(
-                labelText: 'Subject', hintText: 'Math, biology, ...'),
-            onSaved: (value) => setState(() => _newStudyPlan.subject = value),
-            validator: (value) {
-              String answer;
-              if (value.isEmpty) {
-                answer = 'Enter subject';
-              }
-              // todo: add validation.
-              return answer;
-            },
-          ),
-          Text('Date of exam'),
-          InputDatePickerFormField(
-            initialDate: DateTime.now(),
-            firstDate: DateTime(2000),
-            lastDate: DateTime.now().add(Duration(days: 2 * 365)),
-            onDateSaved: (value) => _newStudyPlan.examDate = value,
-          ),
-          RaisedButton(
-            onPressed: () async {
-              final form = _formKey.currentState;
-              if (form.validate()) {
-                form.save();
-                Navigator.of(context).pop();
-                cubit.addStudyPlan(_newStudyPlan);
-              }
-            },
-            child: Text('Create study plan.'),
-          )
-        ],
+      child: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.book),
+              title: TextFormField(
+                decoration: InputDecoration(
+                    labelText: 'Subject', hintText: 'Math, biology, ...'),
+                onSaved: (value) =>
+                    setState(() => _newStudyPlan.subject = value),
+                validator: (value) {
+                  String answer;
+                  if (value.isEmpty) {
+                    answer = 'Enter subject';
+                  }
+                  // todo: add validation.
+                  return answer;
+                },
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.event),
+              title: DateTimeField(
+                format: _format,
+                decoration: InputDecoration(labelText: 'Exam date'),
+                onSaved: (value) => _newStudyPlan.examDate = value,
+                initialValue: _initialDate,
+                onShowPicker: (context, value) =>
+                    showDatePicker(
+                      context: context,
+                      initialDate: _initialDate,
+                      firstDate: _firstDate,
+                      lastDate: _lastDate,
+                    ),
+              ),
+            ),
+            RaisedButton(
+              onPressed: () async {
+                final form = _formKey.currentState;
+                if (form.validate()) {
+                  form.save();
+                  Navigator.of(context).pop();
+                  cubit.addStudyPlan(_newStudyPlan);
+                }
+              },
+              child: Text('Create study plan.'),
+            )
+          ],
+        ),
       ),
     );
   }
