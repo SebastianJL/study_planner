f2990f2508dff699cef5f99b5087850616e5dbe1
SebastianJL <johannes.lade@yahoo.com>
2020-08-19 15:57:59 +0200

    Add a splash screen on startup.

--- android/app/src/main/kotlin/ch/example/study_planner/MainActivity.kt
+++ android/app/src/main/kotlin/ch/example/study_planner/MainActivity.kt
@@ -2,5 +2,8 @@ package ch.example.study_planner
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
